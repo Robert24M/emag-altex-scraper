7cc731047184a118e5efcec2ead04fc40ba0624f
Robert <micu.robert24@gmail.com>
2025-01-06 02:44:41 +0200

    feat: finish EmagPhoneParser

--- src/main/java/me/realprice/emagaltexscraper/parser/EmagPhoneParser.java
+++ src/main/java/me/realprice/emagaltexscraper/parser/EmagPhoneParser.java
@@ -1,6 +1,7 @@
 package me.realprice.emagaltexscraper.parser;
 
 import me.realprice.emagaltexscraper.dto.PhoneDTO;
+import me.realprice.emagaltexscraper.util.PropertiesComputer;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -8,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class EmagPhoneParser {
@@ -17,9 +20,10 @@ public class EmagPhoneParser {
     public static List<PhoneDTO> parse(Document document) {
 
         Elements phoneContainers = document.select(".card-item");
-        List<PhoneDTO> phoneDTOs = new ArrayList<>();
+        List<PhoneDTO> phones = new ArrayList<>();
         for (Element phoneContainer : phoneContainers) {
 
+            PhoneDTO phone = new PhoneDTO();
 //            Element dataNameElement = phoneContainer.selectFirst(".data-name");
 //            if (dataNameElement == null) {
 //                logger.warn("No data-name in element nr {}", phoneContainers.indexOf(phoneContainer));
@@ -30,23 +34,31 @@ public class EmagPhoneParser {
                 logger.warn("No text in dataName, element nr {}", phoneContainers.indexOf(phoneContainer));
                 continue;
             }
-
-            String[] dataNameParts = dataName.split(",");
             // need to compute fields to match PhoneDTO fields
+            PropertiesComputer.computePhonePropertiesEmag(phone, dataName);
 
             String dataURL = phoneContainer.attr("data-url");
             if (dataURL.isEmpty()) {
                 logger.warn("No url, element nr {}", phoneContainers.indexOf(phoneContainer));
             }
+            phone.setUrl(dataURL);
 
             Element priceElement = phoneContainer.selectFirst(".product-new-price");
             if (priceElement == null) {
-                logger.warn("No price element for, element nr {}", phoneContainers.indexOf(phoneContainer));
+                logger.warn("No price element, element nr {}", phoneContainers.indexOf(phoneContainer));
                 continue;
             }
 
             String price = priceElement.ownText();
+
+            if (price.isEmpty()) {
+                logger.warn("No price found, element nr {}", phoneContainers.indexOf(phoneContainer));
+            }
+            phone.setPrice(Double.parseDouble(price));
+
+            phones.add(phone);
         }
-        return phoneDTOs;
+        Collections.sort(phones);
+        return phones;
     }
 }
