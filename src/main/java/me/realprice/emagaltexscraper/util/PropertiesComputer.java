cb34541e59bb00aba3588db57f4d2de9a3418101
Robert <micu.robert24@gmail.com>
2025-01-10 19:13:40 +0200

    feat: start working on computePhonePropertiesEmag method

--- src/main/java/me/realprice/emagaltexscraper/util/PropertiesComputer.java
+++ src/main/java/me/realprice/emagaltexscraper/util/PropertiesComputer.java
@@ -3,9 +3,23 @@ package me.realprice.emagaltexscraper.util;
 
 import me.realprice.emagaltexscraper.dto.PhoneDTO;
 
+import java.util.List;
+
 public class PropertiesComputer {
 
+    private static final List<String> UNWANTED_STRINGS = List.of("Telefon mobil");
+    //TODO: make some constants/enums to match the properties with
+    private static final String NAME_EXAMPLE = "Telefon mobil Samsung Galaxy S24 Ultra, Dual SIM, 12GB RAM, 512GB, 5G, Titanium Black";
+
     public static void computePhonePropertiesEmag(PhoneDTO phoneDTO, String data) {
+
+        for (String unwanted : UNWANTED_STRINGS) {
+            data = data.replace(unwanted, "");
+        }
+
+        data = data.replace("\\s+", " ").trim();
+        String[] dataComponents = data.split(",");
+
 
     }
 }
